547b885ca49a3076d73960627bcdfffdbdd1f899
jpovalles <105830667+jpovalles@users.noreply.github.com>
2025-02-16 19:44:58 -0500

    Added time metrics

--- montepi.cpp
+++ montepi.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib> 
 #include <ctime>
+#include <cmath>
 using namespace std;
 
 int monteCarlo(int nTosses){
@@ -16,13 +17,26 @@ int monteCarlo(int nTosses){
 }
 
 int main(int argc, char* argv[]){
+	clock_t iniCpu = clock();
+    time_t iniReal = time(NULL);
+	
+	
     srand(time(NULL));
     int nTosses = atoi(argv[1]);
 
     int inCircle = monteCarlo(nTosses);
 
     double pi = 4.0 * inCircle / atoi(argv[1]);
-    cout << "Number of tosses in circle " << inCircle << endl;
-    cout << "The value of pi is: " << pi << endl;
+    clock_t finCpu = clock();
+    time_t finReal = time(NULL);
+    double error = fabs(pi - M_PI);
+    
+    cout << "El valor aproximado de pi es: " << pi << endl;
+    
+    cout << "El error es igual a: " << error << endl;
+    double tiempoCpu = (double)(finCpu - iniCpu) / CLOCKS_PER_SEC;
+    double tiempoReal = difftime(finReal, iniReal);
+
+    cout << "Tiempo CPU: " << tiempoCpu << " s, Tiempo Real: " << tiempoReal << " s" << endl;
     return 0;
 }
